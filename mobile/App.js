b8ca887c82fe6c24c26895331ec272960676a274
artenks <silva.arthur@academico.ifpb.edu.br>
2020-01-26 15:24:28 -0300

    chore: get current user location

--- mobile/App.js
+++ mobile/App.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
 
-import MapView from 'react-native-maps'
+import {requestPermission, onCurrentLocationUpdate} from '~/libs/location';
 
 const App = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe = null;
+
+    requestPermission({
+      android: {
+        detail: 'fine',
+      },
+    }).then(granted => {
+      if (granted) {
+        unsubscribe = onCurrentLocationUpdate(locations => {
+          const {longitude, latitude} = locations[0];
+
+          setCurrentLocation({
+            longitude,
+            latitude,
+            longitudeDelta: 0.01,
+            latitudeDelta: 0.01,
+          });
+        });
+      }
+    });
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
+  }, []);
+
+  const handleRegionChange = region => {
+    setCurrentLocation(region);
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
 
       <SafeAreaView style={{flex: 1}}>
-
-        <MapView style={{flex: 1}}
-          initialRegion={{
-            latitude: -6.970255,
-            longitude: -35.7902484,
-            latitudeDelta: 0.02,
-            longitudeDelta: 0.02,
-          }}
-        />
-
+        <MapView
+          style={{flex: 1}}
+          initialRegion={currentLocation}
+          onRegionChange={handleRegionChange}>
+          {currentLocation && (
+            <Marker coordinate={currentLocation}>
+              <Text>Opa</Text>
+            </Marker>
+          )}
+        </MapView>
       </SafeAreaView>
     </>
   );
